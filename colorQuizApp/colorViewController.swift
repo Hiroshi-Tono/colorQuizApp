9235a7c7eaccfcfcb9bf1e9d62c5fdd546cf5226
Hiroshi Tone <valkyrie.gtr@gmail.com>
2018-12-14 12:13:23 +0900

    「2つの画面をつなぐ」まで作成。

---
+++ colorQuizApp/colorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  colorViewController.swift
+//  colorQuizApp
+//
+//  Created by 遠野ひろし on 2018/12/14.
+//  Copyright © 2018 遠野工房. All rights reserved.
+//
+
+import UIKit
+
+class colorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
