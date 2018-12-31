540a71fb7677411dc1aa729fa9e9f3e90ba15c01
Hiroshi Tone <valkyrie.gtr@gmail.com>
2018-12-31 10:52:29 +0900

    Chapter 6-6 色当てクイズアプリのプログラム部分を追加。

--- colorQuizApp/ViewController.swift
+++ colorQuizApp/ViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    // RGBの変数を用意します
+    var colorR = 0
+    var colorG = 0
+    var colorB = 0
+    
+    // この画面が表示されるとき呼び出されます
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 0〜255のランダムな値を3つ作ります
+        colorR = Int(arc4random() % 256)
+        colorG = Int(arc4random() % 256)
+        colorB = Int(arc4random() % 256)
+        // 3つの色を表示します
+        colorLabel.text = "R=\(colorR) ,G=\(colorG), B=\(colorB)"
+    }
+    
+    //画面が切り替わるときに呼び出されます
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 切り替わり先の画面を変数に入れます
+        let nextvc = segue.destination as! colorViewController
+        // 切り替わり先の変数に、この画面の変数を入れて、受け渡します
+        nextvc.colorR = colorR
+        nextvc.colorG = colorG
+        nextvc.colorB = colorB
+    }
+    
     @IBAction func returnTop(segue: UIStoryboardSegue) {
     }
     override func viewDidLoad() {
--- colorQuizApp/colorViewController.swift
+++ colorQuizApp/colorViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class colorViewController: UIViewController {
 
+    // RGBの変数を用意します
+    var colorR = 0
+    var colorG = 0
+    var colorB = 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // RGBから色を作ります
+        let backColor = UIColor(red: CGFloat(colorR)/256.0,
+                                green: CGFloat(colorG)/256.0,
+                                blue: CGFloat(colorB)/256.0,
+                                alpha: 1.0)
+        // 背景色を設定します
+        view.backgroundColor = backColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
